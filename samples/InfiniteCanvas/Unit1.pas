c26041e807332e5bb30ac56eb35b72af85e9c7e7
hjF <hjfactory@gmail.com>
2012-06-19 16:44:20 +0900

    Complete InfiniteCanvas

---
+++ samples/InfiniteCanvas/Unit1.pas
@@ -0,0 +1,27 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.Objects, FMX.Layouts;
+
+type
+  TForm1 = class(TForm)
+    ScrollBox1: TScrollBox;
+    Rectangle1: TRectangle;
+    Rectangle2: TRectangle;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
--- samples/InfiniteCanvas/Unit4.pas
+++ samples/InfiniteCanvas/Unit4.pas
@@ -14,28 +14,39 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.Layouts, FMX.Memo, FMX.Objects,
-  Unit5;
+  Unit5, Unit1;
 
 type
   TForm4 = class(TForm)
     Panel1: TPanel;
+    ScrollBox1: TScrollBox;
+    StyleBook1: TStyleBook;
+    Rectangle1: TRectangle;
     Panel2: TPanel;
     Button1: TButton;
-    Button2: TButton;
-    Button3: TButton;
-    ScrollBox1: TScrollBox;
-    Panel3: TPanel;
-    Button4: TButton;
-    StyleBook1: TStyleBook;
+    Rectangle2: TRectangle;
+    Rectangle3: TRectangle;
+    Rectangle4: TRectangle;
+    Rectangle5: TRectangle;
+    Rectangle6: TRectangle;
+    Rectangle7: TRectangle;
+    Rectangle8: TRectangle;
+    Rectangle9: TRectangle;
+    Rectangle10: TRectangle;
     procedure ScrollBox1Paint(Sender: TObject; Canvas: TCanvas;
       const ARect: TRectF);
-    procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure Button4Click(Sender: TObject);
-    procedure Button3Click(Sender: TObject);
+    procedure ScrollBox1MouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Single);
+    procedure ScrollBox1MouseMove(Sender: TObject; Shift: TShiftState; X,
+      Y: Single);
+    procedure ScrollBox1MouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Single);
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
-    FCanvas: TThothCanvas;
+    FOffSet, FPos: TPointF;
+    FDown: Boolean;
   public
     { Public declarations }
   end;
@@ -52,67 +63,118 @@ uses
 
 procedure TForm4.Button1Click(Sender: TObject);
 begin
-  with TRectangle.Create(ScrollBox1) do
-  begin
-    Parent := ScrollBox1;
-    Width := 100;
-    height := 100;
-    Position.Point := PointF(10, 10);
-  end;
-
-  ScrollBox1.Repaint;
-end;
-
-procedure TForm4.Button3Click(Sender: TObject);
-begin
-  OutputDebugString(PChar(Format('%f %f', [FCanvas.HScrollBar.Value, FCanvas.VScrollBar.Value])));
-end;
-
-procedure TForm4.Button4Click(Sender: TObject);
-begin
-  with TRectangle.Create(FCanvas) do
-  begin
-    Parent := FCanvas;
-    Width := 100;
-    height := 100;
-    Position.Point := PointF(10, 10);
-  end;
-
-  FCanvas.Realign;
-  FCanvas.Center;
+  Form1.Show;
 end;
 
 procedure TForm4.FormCreate(Sender: TObject);
 begin
-  ScrollBox1.HScrollBar.Value := 10000;
+  FOffSet := PointF(0, 0);
 
-  FCanvas := TThothCanvas.Create(Panel3);
-  FCanvas.Align := TAlignLayout.alClient;
-  FCanvas.Parent := Panel3;
+//  ScrollBox1.HScrollBar.Value := 10000;
+end;
 
-//  FCanvas.StyleName := 'TScrollBoxstyle';
-  FCanvas.StyleLookup := 'ScrollBoxstyle';
+procedure TForm4.ScrollBox1MouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Single);
+begin
+  // ¹Ù´Ú¼±ÅÃ(°´Ã¼ ¼±ÅÃ¾ÈÇÔ)
+  if Button = TMouseButton.mbLeft then
+  begin
+    FPos := PointF(X, Y);
+    FDown := True;
+  end;
+end;
 
-  FCanvas.HScrollBar.Min := -1000;
-  FCanvas.HScrollBar.Max := 1000;
-  FCanvas.VScrollBar.Min := -1000;
-  FCanvas.VScrollBar.Max := 1000;
+procedure TForm4.ScrollBox1MouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Single);
+var
+  I: Integer;
+  MovePos: TPointF;
+  s: TShape;
+begin
+//  Exit;
+  if FDown and (ssLeft in Shift) then
+  begin
+//    OutputDebugString(PChar(Format('FPos: %f, %f', [FPos.X, FPos.Y])));
+//    OutputDebugString(PChar(Format('X/Y: %f, %f', [X, Y])));
+    MovePos.SetLocation(X-FPos.X, Y-FPos.Y);
+    FOffSet.Offset(MovePos);
+    FPos := PointF(X, Y);
+//    OutputDebugString(PChar(Format('OffSet: %f, %f', [FOffSet.X, FOffSet.Y])));
 
-  FCanvas.HScrollBar.Visible := True;
-  FCanvas.VScrollBar.Visible := True;
-  FCanvas.Realign;
+//    OutputDebugString(PChar(Format('B: %f, %f', [Rectangle1.Position.X, Rectangle1.Position.Y])));
+
+//    Rectangle1.Position.Point.Offset(FOffSet);
+    for I := 0 to ScrollBox1.Children[1].ChildrenCount - 1 do
+    begin
+      if ScrollBox1.Children[1].Children[I] is TShape then
+      begin
+
+        s := TShape(ScrollBox1.Children[1].Children[I]);
+        s.Position.X := s.Position.X + MovePos.X;
+        s.Position.Y := s.Position.Y + MovePos.Y;
+      end;
+    end;
+
+//    Rectangle1.Position.X := Rectangle1.Position.X + MovePos.X;
+//    Rectangle1.Position.Y := Rectangle1.Position.Y + MovePos.Y;
+
+//    OutputDebugString(PChar(Format('A: %f, %f', [Rectangle1.Position.X, Rectangle1.Position.Y])));
+//    for I := 0 to ScrollBox1.ChildrenCount - 1 do
+//    begin
+//      TShape(ScrollBox1.Children[I]).Position.Point.Offset(FOffSet);
+//    end;
+
+    ScrollBox1.Repaint;
+  end;
+end;
+
+procedure TForm4.ScrollBox1MouseUp(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Single);
+var
+  MovePos: TPointF;
+begin
+  Exit;
+  if FDown then
+  begin
+    OutputDebugString(PChar(Format('FPos: %f, %f', [FPos.X, FPos.Y])));
+    OutputDebugString(PChar(Format('X/Y: %f, %f', [X, Y])));
+    MovePos.SetLocation(X-FPos.X, Y-FPos.Y);
+    FOffSet.Offset(MovePos);
+    FPos := PointF(X, Y);
+    OutputDebugString(PChar(Format('OffSet: %f, %f', [FOffSet.X, FOffSet.Y])));
+
+    OutputDebugString(PChar(Format('B: %f, %f', [Rectangle1.Position.X, Rectangle1.Position.Y])));
+
+//    scrollBox1.
+
+//    Rectangle1.Position.Point.Offset(FOffSet);
+    Rectangle1.Position.X := Rectangle1.Position.X + MovePos.X;
+    Rectangle1.Position.Y := Rectangle1.Position.Y + MovePos.Y;
+
+    OutputDebugString(PChar(Format('A: %f, %f', [Rectangle1.Position.X, Rectangle1.Position.Y])));
+//    for I := 0 to ScrollBox1.ChildrenCount - 1 do
+//    begin
+//      TShape(ScrollBox1.Children[I]).Position.Point.Offset(FOffSet);
+//    end;
+
+    ScrollBox1.Repaint;
+  end;
 end;
 
 procedure TForm4.ScrollBox1Paint(Sender: TObject; Canvas: TCanvas;
   const ARect: TRectF);
 var
   I: Integer;
+  X, Y: single;
 begin
+  X := Trunc(FOffSet.X) mod 100;
+  Y := Round(FOffSet.Y) mod 100;
+
   for I := 0 to (Trunc(ScrollBox1.Width-1) div 100) do
-    Canvas.DrawLine(PointF(100 * I, 0), PointF(100 * I, ScrollBox1.Height), 1);
+    Canvas.DrawLine(PointF(X + 100 * I, 0), PointF(X + 100 * I, ScrollBox1.Height), 1);
 
   for I := 0 to (Trunc(ScrollBox1.Height-1) div 100) do
-    Canvas.DrawLine(PointF(0, 100 * I), PointF(ScrollBox1.Width, 100 * I), 1);
+    Canvas.DrawLine(PointF(0, Y + 100 * I), PointF(ScrollBox1.Width, Y + 100 * I), 1);
 end;
 
 end.
