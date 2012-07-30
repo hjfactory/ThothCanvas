81291343d0eee6f467e0c1a9d77dcfc3effcc35b
hjF <hjfactory@gmail.com>
2012-07-30 17:26:31 +0900

    ThCanvas 초안작업 진행 중

--- samples/Elements/ThCanvas/Main.pas
+++ samples/Elements/ThCanvas/Main.pas
@@ -5,16 +5,21 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms,
-  ThothCanvas, FMX.Layouts;
+  ThothCanvas, FMX.Layouts, FMX.Dialogs;
 
 type
   TForm1 = class(TForm)
     Panel1: TPanel;
     Button1: TButton;
     Button2: TButton;
+    ScrollBox1: TScrollBox;
+    Button3: TButton;
+    Button4: TButton;
     procedure FormCreate(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
   private
     { Private declarations }
     FThCanvas: TThCanvas;
@@ -39,8 +44,26 @@ begin
   end;
 end;
 
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  ShowMessage(Format('%s', [FThCanvas.test]));
+end;
+
+procedure TForm1.Button4Click(Sender: TObject);
+begin
+  with TButton.Create(FThCanvas) do
+  begin
+    Parent := FThCanvas;
+    Position.Point := PointF(-100, -110);
+    Text := 'asdfsaf';
+  end;
+
+end;
+
 procedure TForm1.FormCreate(Sender: TObject);
 begin
+//  Application.DefaultStyles
+
   FThCanvas := TThCanvas.Create(Self);
   FThCanvas.Align := TAlignLayout.alClient;
   FThCanvas.Parent := Panel1;
--- samples/Elements/ThCanvas/ThothCanvas.pas
+++ samples/Elements/ThCanvas/ThothCanvas.pas
@@ -3,24 +3,99 @@ unit ThothCanvas;
 interface
 
 uses
-  System.Classes, FMX.Types;
+  System.Classes, System.SysUtils, System.Types, System.UITypes, FMX.Types;
 
 type
   TThContent = class(TControl)
+    // Layout - ClipRect
+    // OffSet - Point(Position)
+  private
+    FLayout: TControl;
+    FOffset: TPointF;
+    FTrackingPos: TPointF;
+    FSize: TPointF;
 
+    procedure SetTrackingPos(const Value: TPointF);
+    procedure SetSize(const Value: TPointF);
+  protected
+    constructor Create(AOwner: TComponent); override;
+
+    function GetClipRect: TRectF; override;
+//    function GetUpdateRect: TRectF; override;
+
+    property TrackingPos: TPointF read FTrackingPos write SetTrackingPos;
+    property Size: TPointF read FSize write SetSize;
+
+    procedure test;
   end;
 
   TThCanvas = class(TStyledControl)
   private
+    FDown: Boolean;
+    FCurrentPos: TPointF;
+
     FContent: TThContent;
+    FMouseTracking: Boolean;
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
 
     procedure AddObject(AObject: TFmxObject); override;
+
+    procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Single); override;
+    procedure MouseMove(Shift: TShiftState; X, Y: Single); override;
+    procedure MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Single); override;
+
+    property MouseTracking: Boolean read FMouseTracking write FMouseTracking default False;
+
+    function test: string;
+
+    procedure Realign; override;
   end;
 
 implementation
+
+{ TThContent }
+
+constructor TThContent.Create(AOwner: TComponent);
+begin
+  inherited;
+end;
+
+function TThContent.GetClipRect: TRectF;
+begin
+
+  Result := RectF(0, 0, Size.X, Size.Y);
+  OffsetRect(Result, Position.X, Position.Y);
+
+//  if (Parent <> nil) and (Parent is TThContent) and  then
+
+// Update 할 영역만 반환
+end;
+
+procedure TThContent.SetSize(const Value: TPointF);
+begin
+  FSize := Value;
+end;
+
+procedure TThContent.SetTrackingPos(const Value: TPointF);
+begin
+  FTrackingPos := Value;
+
+  Position.X := Position.X + Value.X;
+  Position.Y := Position.Y + Value.Y;
+
+//  Self.SetBoundsRect(ClipRect);
+
+//  Self.SetBoundsRect();
+end;
+
+procedure TThContent.test;
+var
+  R: TRectF;
+begin
+  SetBoundsRect(ClipRect);
+end;
 
 { TThCanvas }
 
@@ -28,11 +103,14 @@ constructor TThCanvas.Create(AOwner: TComponent);
 begin
   inherited;
 
+  FMouseTracking := True;
+
   FContent := TThContent.Create(Self);
   FContent.Parent := Self;
   FContent.Stored := False;
   FContent.Locked := True;
   FContent.HitTest := False;
+  FContent.Size := PointF(Width, Height);
 end;
 
 destructor TThCanvas.Destroy;
@@ -41,6 +119,72 @@ begin
   FContent := nil;
 
   inherited;
+end;
+
+procedure TThCanvas.MouseDown(Button: TMouseButton; Shift: TShiftState; X,
+  Y: Single);
+begin
+  inherited;
+
+  if (Button = TMouseButton.mbLeft) and FMouseTracking then
+  begin
+    FCurrentPos := PointF(X, Y);
+    FDown := True;
+  end;
+end;
+
+procedure TThCanvas.MouseMove(Shift: TShiftState; X, Y: Single);
+begin
+  inherited;
+
+  if FDown and FMouseTracking then
+  begin
+    FContent.TrackingPos := PointF(X - FCurrentPos.X, Y - FCurrentPos.Y);
+
+    FCurrentPos := PointF(X, Y);
+  end;
+end;
+
+procedure TThCanvas.MouseUp(Button: TMouseButton; Shift: TShiftState; X,
+  Y: Single);
+begin
+  inherited;
+
+  if FDown and FMouseTracking then
+  begin
+    FDown := False;
+  end;
+end;
+
+procedure TThCanvas.Realign;
+
+  procedure IntAlign;
+  begin
+    FContent.test;
+  end;
+begin
+  if csDestroying in ComponentState then
+    Exit;
+  inherited;
+  if csLoading in ComponentState then
+    Exit;
+
+  if FDisableAlign then
+    Exit;
+  if FUpdating > 0 then
+    Exit;
+  FDisableAlign := True;
+  try
+    IntAlign;
+  finally
+    FDisableAlign := False;
+  end;
+end;
+
+function TThCanvas.test: string;
+begin
+  Result := 'X: ' + IntToStr(Round(FContent.Width)) + ', Y: ' + IntToStr(Round(FContent.Height));
+  Result := 'X: ' + IntToStr(Round(FContent.Position.X)) + ', Y: ' + IntToStr(Round(FContent.Position.Y));
 end;
 
 procedure TThCanvas.AddObject(AObject: TFmxObject);
