84da16afca0b8f8cffe32948503cf908aa3f5c62
hjf-Mac <hjfactory@gmail.com>
2012-09-26 01:01:16 +0900

    Test library.

--- Source/src/Unittest/lib/FMX.TestLib.Win.pas
+++ Source/src/Unittest/lib/FMX.TestLib.Win.pas
@@ -1,7 +1,93 @@
+{
+
+  Reference
+    - mouse_event : http://msdn.microsoft.com/en-us/library/windows/desktop/ms646260(v=vs.85).aspx
+    - keybd_event : http://msdn.microsoft.com/en-us/library/windows/desktop/ms646304(v=vs.85).aspx
+}
+
 unit FMX.TestLib.Win;
 
 interface
 
+uses
+  System.Types, System.UITypes, System.Classes, FMX.TestLib;
+
+function GetTestLibClass: TTestLibClass;
+
 implementation
+
+uses
+  Winapi.Windows, Winapi.ShellAPI, FMX.Platform;
+
+type
+  TTestLibWin = class(TTestLib)
+  public
+    procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Single); override;
+    procedure MouseMove(Shift: TShiftState; X, Y: Single); override;
+    procedure MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Single); override;
+    procedure MouseWheel(Shift: TShiftState; WheelDelta: Integer; var Handled: Boolean); override;
+    procedure KeyDown(var Key: Word; var KeyChar: WideChar; Shift: TShiftState); override;
+    procedure KeyUp(var Key: Word; var KeyChar: WideChar; Shift: TShiftState); override;
+  end;
+
+function GetTestLibClass: TTestLibClass;
+begin
+  Result := TTestLibWin;
+end;
+
+{ TTestLibWin }
+
+procedure TTestLibWin.KeyDown(var Key: Word; var KeyChar: WideChar;
+  Shift: TShiftState);
+begin
+
+end;
+
+procedure TTestLibWin.KeyUp(var Key: Word; var KeyChar: WideChar;
+  Shift: TShiftState);
+begin
+
+end;
+
+procedure TTestLibWin.MouseDown(Button: TMouseButton; Shift: TShiftState; X,
+  Y: Single);
+var
+  Flag: DWORD;
+begin
+  case Button of
+    TMouseButton.mbLeft: Flag := MOUSEEVENTF_LEFTDOWN;
+    TMouseButton.mbRight: Flag := MOUSEEVENTF_RIGHTDOWN;
+    TMouseButton.mbMiddle: Flag := MOUSEEVENTF_MIDDLEDOWN;
+  end;
+
+  SetCursorPos(Round(X), Round(Y));
+  mouse_event(Flag, Round(X), Round(Y), 0, 0);
+end;
+
+procedure TTestLibWin.MouseMove(Shift: TShiftState; X, Y: Single);
+begin
+  mouse_event(MOUSEEVENTF_MOVE, Round(X), Round(Y), 0, 0);
+end;
+
+procedure TTestLibWin.MouseUp(Button: TMouseButton; Shift: TShiftState; X,
+  Y: Single);
+var
+  Flag: DWORD;
+begin
+  case Button of
+    TMouseButton.mbLeft: Flag := MOUSEEVENTF_LEFTUP;
+    TMouseButton.mbRight: Flag := MOUSEEVENTF_RIGHTUP;
+    TMouseButton.mbMiddle: Flag := MOUSEEVENTF_MIDDLEUP;
+  end;
+
+  SetCursorPos(Round(X), Round(Y));
+  mouse_event(Flag, Round(X), Round(Y), 0, 0);
+end;
+
+procedure TTestLibWin.MouseWheel(Shift: TShiftState; WheelDelta: Integer;
+  var Handled: Boolean);
+begin
+
+end;
 
 end.
--- Source/src/Unittest/lib/FMX.TestLib.pas
+++ Source/src/Unittest/lib/FMX.TestLib.pas
@@ -3,67 +3,40 @@ unit FMX.TestLib;
 interface
 
 uses
-  System.Types, System.UITypes, FMX.Types, FMX.Forms, FMX.Platform.Win;
+  System.Types, System.UITypes, System.Classes, FMX.Types, FMX.Forms, FMX.Platform.Win;
 
 type
-  TTestLib = class
-
+  TTestLib = class(TObject)
+  public
+    procedure MouseDown(Button: TMouseButton; Shift: TShiftState; X, Y: Single); virtual; abstract;
+    procedure MouseMove(Shift: TShiftState; X, Y: Single); virtual; abstract;
+    procedure MouseUp(Button: TMouseButton; Shift: TShiftState; X, Y: Single); virtual; abstract;
+    procedure MouseWheel(Shift: TShiftState; WheelDelta: Integer; var Handled: Boolean); virtual; abstract;
+    procedure KeyDown(var Key: Word; var KeyChar: WideChar; Shift: TShiftState); virtual; abstract;
+    procedure KeyUp(var Key: Word; var KeyChar: WideChar; Shift: TShiftState); virtual; abstract;
   end;
 
-procedure MouseDown(const P: TPointF);
-procedure MouseUp(const P: TPointF);
-procedure MouseClick(const P: TPointF);
+  TTestLibClass = class of TTestLib;
 
-function TestLib: TTestLib;
+var
+  TestLib: TTestLib;
 
 implementation
 
 uses
-  FMX.Platform;
-
-function TestLib: TTestLib;
-begin
 {$IFDEF MACOS}
+  FMX.TestLib.Mac;
 {$ENDIF}
 
 {$IFDEF MSWINDOWS}
+  FMX.TestLib.Win;
 {$ENDIF}
-end;
 
-function GetObjInPoint(const P: TPointF): IControl;
-begin
-//  Result := IControl(ObjectAtPoint(ClientToScreen(P)));
-  if Assigned(Application.MainForm) then
-  begin
+initialization
+  TestLib := GetTestLibClass.Create;
 
-  end;
-
-end;
-
-
-procedure MouseDown(const P: TPointF);
-var
-  Pt: TPointF;
-  Obj: IControl;
-begin
-  Obj := GetObjInPoint(P);
-  if Assigned(Obj) then
-  begin
-    Pt := Obj.LocalToScreen(P);
-    Pt := Obj.ScreenToLocal(Pt);
-    Obj.MouseDown(TMouseButton.mbLeft, [], P.X, P.Y);
-    Obj.MouseUp(TMouseButton.mbLeft, [], P.X, P.Y);
-  end;
-end;
-
-procedure MouseUp(const P: TPointF);
-begin
-
-end;
-
-procedure MouseClick(const P: TPointF);
-begin
-
-end;
+finalization
+  if Assigned(TestLib) then
+    TestLib.Free;
 
 end.
